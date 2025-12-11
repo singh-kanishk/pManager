7000d92b23fbbbd68521898e191500645dedf38a
singh-kanishk <skanishk6124@gmail.com>
2025-12-11 16:30:08 +0530

    tested cors and fetch

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 const PORT=3998;
 const app = express();
-
+app.use(cors())
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send('hello');
+})
+app.post('/api/data',(req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
 })
 app.listen(PORT,()=>{
     console.log(`is Listening at ${PORT}`)
--- src/hp__script.js
+++ src/hp__script.js
@@ -1,14 +1,25 @@
 const newAdditionButton = document.querySelector(".new-button");
 newAdditionButton.addEventListener("click", () => {
-  document.querySelector(".modal.modal-newAddition").classList.toggle("show");
+  document.querySelector(".modal.modal-newAddition").classList.add("show");
 });
-const cancelOnAdditionForm=document.querySelector('.modal-close')
+const cancelOnAdditionForm = document.querySelector(".modal-close");
 cancelOnAdditionForm.addEventListener("click", () => {
-  document.querySelector(".modal.modal-newAddition").classList.toggle("show");
-})
-const submitAdditionForm=document.getElementById('#newAddition_save')
-submitAdditionForm.addEventListener('submit', async (e)=>{
+  document.querySelector(".modal.modal-newAddition").classList.remove("show");
+});
+const submitAdditionForm = document.querySelector('.newAddition');
+submitAdditionForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const formData=new FormData(submitAdditionForm);
-  const data=Object.fromEntries(formData);
-})
+  const formData = new FormData(submitAdditionForm);
+  const data = Object.fromEntries(formData);
+  try {
+    const response = await fetch("http://localhost:3998/api/data", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log(`Error is ${e}`);
+  }
+});
